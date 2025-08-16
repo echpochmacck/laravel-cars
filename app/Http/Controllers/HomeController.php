668e46ee1e7ab055a057ab770accd78c1191ba23
Storm <max22062006@gmail.com>
2025-08-16 21:12:48 +0300

    init

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        // return 'index';
+        return view('home.index');
+        // return View::make('home.about');
+    }
+}
